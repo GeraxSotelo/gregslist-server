8f5189130d427568509a71bd9fd20f69da9d9da0
Gerax Sotelo <gsotelo23@gmail.com>
2019-12-10 20:50:02 -0700

    create job service and add methods

---
+++ server/services/JobService.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+import Job from "../models/Jobs"
+
+const _repository = mongoose.model("Job", Job)
+
+class JobService {
+  async getAll() {
+    return await _repository.find({})
+  }
+
+  async getById(id) {
+    let data = await _repository.findById(id);
+    if (!data) {
+      throw new Error("Invalid ID")
+    }
+    return data
+  }
+
+  async create(body) {
+    return await _repository.create(body) {
+    }
+  }
+
+  async edit(id, body) {
+    let data = await _repository.findOneAndUpdate(id, body, { new: true });
+    if (!data) {
+      throw new Error("Invalid ID")
+    }
+    return data
+  }
+
+  async delete(id) {
+    let data = await _repository.findOneAndDelete(id)
+    if (!data) {
+      throw new Error("Invalid ID")
+    }
+  }
+
+}
+
+const jobService = new JobService();
+export default jobService;
